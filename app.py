490236ec107ac90522660d88b1fc047c30995cf3
Harshil Patel <harshilpatel30401@gmail.com>
2022-02-24 22:57:12 +0530

    Correct one

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ from gettext import install
 import dash
 
 # import dash_html_components as html
-# import dae'], y = df['GOOG'],\
+# import dash_core_components as dcc
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+import plotly.express as px
+
+app = dash.Dash()   #initialising dash app
+df = px.data.stocks() #reading stock price dataset 
+    
+def stock_prices():
+    # Function for creating line chart showing Google stock prices over time 
+    fig = go.Figure([go.Scatter(x = df['date'], y = df['GOOG'],\
                      line = dict(color = 'firebrick', width = 4), name = 'Google')
                      ])
     fig.update_layout(title = 'Prices over time',
@@ -13,6 +24,12 @@ import dash
     return fig  
 
 app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Styling using html components this is harshil', style = {'textAlign':'center',\
-                                           
+    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}),
+
+        
+        dcc.Graph(id = 'line_plot', figure = stock_prices())    
+    ]
+                     )
+
 app.run_server(debug=True, port=1000)

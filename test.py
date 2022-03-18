4f1be2184b2f33421aeb4220a259080e78b91350
harshil30401 <harshilpatel30401@gmail.com>
2022-03-18 22:55:33 +0530

    Test commit for branch 2

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-import os
-cityName = "Delhi"
-BASE_DIR = str((os.path.dirname(os.path.dirname(os.path.abspath(__file__))))).replace('\\', '/')
-
-file = f"{BASE_DIR}/Air-Quality-Index-Prediction/datasets/{cityName}.csv"
-print(file)
-
+print("This is branch 2")

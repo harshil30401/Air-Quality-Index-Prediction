76bf55cd2f7e4082811b518ba3f21e8057a04c3a
harshil30401 <harshilpatel30401@gmail.com>
2022-03-18 23:36:12 +0530

    Final commit for update branch

--- index.py
+++ index.py
@@ -95,7 +95,7 @@ def display_page(pathname):
         return visakhapatnam.layout
 
     else:
-        return "404 Page Error! Please choose a link"
+        return "404 Page Error! Please choose a valid city"
 
 if __name__ == '__main__':
     app.run_server(debug=True)

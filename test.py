700243b864d3090000fdd5e3790e621aefeffa69
harshil30401 <harshilpatel30401@gmail.com>
2022-03-20 21:36:03 +0530

    Testing

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-cityName = "Delhi"
-BASE_DIR = str((os.path.dirname(os.path.dirname(os.path.abspath(__file__))))).replace('\\', '/')
-
-file = f"{BASE_DIR}/Air-Quality-Index-Prediction/datasets/{cityName}.csv"
-print(file)
-

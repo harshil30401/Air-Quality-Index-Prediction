61148b0439c8def57b7f2b339ef84fd572c7a4cf
Usashiroy <usashiroy57@gmail.com>
2022-04-03 23:36:08 +0530

    Started with data analysis

---
+++ dataAnalysis.py
@@ -0,0 +1,17 @@
+import bar_chart_race as bcr
+import pandas as pd
+# from IPython.display import HTML
+import numpy as np
+import seaborn as sns
+from rootInformation import rootDirectory
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import folium
+
+cities = pd.read_excel(f"{rootDirectory}/Air-Quality-Index-Prediction/datasets/allCities.xlsx")
+cities['Date']=pd.to_datetime(cities['Date'])
+
+cities['PM'] = cities['PM2.5'] + cities['PM10']
+
+
--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-import pandas as pd
 from rootInformation import rootDirectory
 
-citiesMean = pd.read_csv(f"{rootDirectory}/Air-Quality-Index-Prediction/datasets/citiesMean.csv")
-
-cityAQI = dict(zip(citiesMean.City, citiesMean.AQI))
-
-city = "Mumbai"
-
-# for c in cityAQI:
-#     if c.keys() == city:
-#         print(c.values())
-
-print(cityAQI[city])
+print(rootDirectory)
